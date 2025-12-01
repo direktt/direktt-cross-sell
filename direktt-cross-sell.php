395cd1f703d818da4a254f02867b2340aa86319e
ilijaStanisavljevic <ilija.stanisavljevic2004@gmail.com>
2025-12-01 15:48:06 +0100

    x

--- direktt-cross-sell.php
+++ direktt-cross-sell.php
@@ -56,7 +56,9 @@ add_action( 'direktt_setup_profile_tools', 'direktt_cross_sell_setup_profile_too
 
 // Reports ajax handlers
 add_action( 'wp_ajax_direktt_cross_sell_get_issued_report', 'handle_direktt_cross_sell_get_issued_report' );
+add_action( 'wp_ajax_nopriv_direktt_cross_sell_get_issued_report', 'handle_direktt_cross_sell_get_issued_report' );
 add_action( 'wp_ajax_direktt_cross_sell_get_used_report', 'handle_direktt_cross_sell_get_used_report' );
+add_action( 'wp_ajax_nopriv_direktt_cross_sell_get_used_report', 'handle_direktt_cross_sell_get_used_report' );
 
 // [direktt_cross_sell_coupon_validation] shortcode implementation
 add_shortcode( 'direktt_cross_sell_coupon_validation', 'direktt_cross_sell_coupon_validation' );
@@ -1450,7 +1452,7 @@ function direktt_cross_sell_get_partner_coupon_groups( $partner_id ) {
 }
 
 function direktt_cross_sell_user_can_review() {
-	global $direktt_user;
+	$direktt_user = Direktt_User::direktt_get_current_user();;
 
 	// Always allow admin
 	if ( class_exists( 'Direktt_User' ) && Direktt_User::is_direktt_admin() ) {
@@ -1513,7 +1515,7 @@ function direktt_cross_sell_setup_profile_tool() {
 
 function direktt_cross_sell_process_use_coupon( $coupon ) {
 	// TODO testirati ovo
-	global $direktt_user;
+	$direktt_user = Direktt_User::direktt_get_current_user();;
 	if ( ! $direktt_user ) {
 		return;
 	}
@@ -2232,7 +2234,7 @@ function direktt_cross_sell_render_issued( $subscription_id ) {
 
 function direktt_cross_sell_my_coupons() {
 
-	global $direktt_user;
+	$direktt_user = Direktt_User::direktt_get_current_user();;
 	global $wpdb;
 
 	$subscription_id = $direktt_user['direktt_user_id'];
@@ -2420,7 +2422,7 @@ function direktt_cross_sell_get_partner_and_group_by_coupon_guid( $coupon_guid )
 }
 
 function direktt_cross_sell_user_can_validate( $partner_id ) {
-	global $direktt_user;
+	$direktt_user = Direktt_User::direktt_get_current_user();;
 
 	// Always allow admin
 	if ( class_exists( 'Direktt_User' ) && Direktt_User::is_direktt_admin() ) {
@@ -2685,7 +2687,7 @@ function direktt_cross_sell_render_profile_tool() {
 }
 
 function direktt_cross_sell_user_tool() {
-	global $direktt_user;
+	$direktt_user = Direktt_User::direktt_get_current_user();;
 	global $wpdb;
 	$table = $wpdb->prefix . 'direktt_cross_sell_issued';
 	$now   = current_time( 'mysql' );
@@ -3104,7 +3106,7 @@ function direktt_cross_sell_user_tool() {
 }
 
 function direktt_cross_sell_on_issue_coupon( $request ) {
-	global $direktt_user;
+	$direktt_user = Direktt_User::direktt_get_current_user();;
 	$direktt_receiver_user_id = $direktt_user['direktt_user_id'] ?? '';
 	$coupon_group_id          = intval( $request['coupon_group_id'] ?? 0 );
 	$partner_id               = intval( $request['partner_id'] ?? 0 );
